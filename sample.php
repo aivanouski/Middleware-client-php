1122cf39dd81a23d55ff5ed7da65150b149b694a
zyevegor <zyev.egor@gmail.com>
2017-07-13 08:53:08 +0300

    https://chronobank.atlassian.net/projects/MINT/issues/MINT-329 - Middleware php api

--- sample.php
+++ sample.php
@@ -4,9 +4,11 @@ include "MiddlewareClient.php";
 
 $client = new MiddlewareClient("http://localhost:8081");
 
-$query = $client->getEvents('done', json_decode('{"self": "0x096ee6177d6402979b5e3f17385c36231133c254"}'), null);
-$query2 = $client->addAccount("0x096ee6177d6402979b5e3f17385c36231133c254");
+$query_event = $client->getEvents('done', json_decode('{"self": "0x096ee6177d6402979b5e3f17385c36231133c254"}'), null);
+$query_tx = $client->getTransactions(json_decode('{"to": "0x62d0a3919435a7c10ae5e63c230a7c9da3b66e45"}'), null);
+$query_acc = $client->addAccount("0x096ee6177d6402979b5e3f17385c36231133c254");
 
-var_dump($query);
-var_dump($query2);
+var_dump($query_event);
+var_dump($query_tx);
+var_dump($query_acc);
 

117c82cd8070834b3a674a30b1da894968a22d0d
zyevegor <zyev.egor@gmail.com>
2017-07-27 14:44:53 +0300

    https://chronobank.atlassian.net/projects/MINT/issues/MINT-329 - Middleware php api

--- api/addFilter.php
+++ api/addFilter.php
@@ -1,18 +1,26 @@
 <?php
 
-function addFilter($host, $callback, $event, $filter){
+function addFilter($host, $callback, $event, $filter) {
+
     $ch = curl_init();
+
+    $data = array("callback" => $callback, "event" => $event, "filter" => $filter);
+    $data_string = json_encode($data);
+    echo $data_string;
+
+
     curl_setopt($ch, CURLOPT_URL, $host . "/events/listener");
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,
-        "callback=". $callback);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,
-        "event=". $event);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,
-        "filter=". $filter);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data_string))
+    );
     $data = curl_exec($ch);
     curl_close($ch);
     return json_decode($data);
 
-};
+}
+
+;
